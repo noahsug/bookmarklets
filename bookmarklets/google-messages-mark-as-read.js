c38e9cfa4f928b088059066cdea104b504dbe782
noah.sugarman <noah.sugarman@airbnb.com>
2021-06-15 16:28:21 -0700

    [messages.google.com] mark all as read

--- bookmarklets/google-messages-mark-as-read.js
+++ bookmarklets/google-messages-mark-as-read.js
@@ -1,39 +1,36 @@
 const ORIGIN_REGEX = new RegExp('//messages.google.com');
 if (!ORIGIN_REGEX.test(document.location.origin)) {
-  console.error('Cannot run bookmarklet! URL should match', ORIGIN_REGEX);
+  alert(`Cannot run bookmarklet!\nURL should match "${ORIGIN_REGEX}"`);
   process.exit(1);
 }
 
-// delay after clicking on a message
+// milliseconds to wait after clicking on a message
 const CLICK_DELAY = 500;
-// delay after scrolling to an already loaded part of the page
+// milliseconds to wait after scrolling to an already loaded part of the page
 const SCROLL_DELAY = 500;
-// delay after loading in new messages
-const LOAD_DELAY = 2500;
+// milliseconds to wait before giving up on loading more messages
+const LOAD_DELAY = 3000;
 
 const selectors = {
   unreadItem: '.text-content.unread',
   item: '.text-content',
 };
 
-async function iterate({ prevNumItems, loadCount } = { prevNumItems: 0, loadRetryCount: 0 }) {
+async function iterate() {
   await markAllInViewAsRead();
 
   const items = selectAllOrError(selectors.item);
+  await loadMoreItems(items);
+  const newItems = selectAllOrError(selectors.item);
 
-  // check if there are any more items to scroll to
-  if (ite || loadCount < LOAD_ATTEMPTS) {
-    console.log('loading new messages...', loadCount > 0 ? `(attempt ${loadCount + 1})` : '');
-    await scrollInMoreItems(items);
-
-    const newLoadCount = lastItem === prevLastItem && markedAsRead === 0 ? loadCount + 1 : 0;
-    await iterate(lastItem, newLoadCount);
+  // run until there are no new items to load
+  if (newItems.length > items.length) {
+    await iterate();
   }
 }
 
 function markAllInViewAsRead() {
   const unread = document.querySelectorAll(selectors.unreadItem);
-
   if (unread.length > 0) {
     console.log('marking', unread.length, 'messages as read');
   }
@@ -48,17 +45,24 @@ function markAllInViewAsRead() {
   return runSequentially(markAsUnreadJobs);
 }
 
-async function scrollInMoreItems(items) {
-  const lastItem = items[items.length - 1];
-  const firstItem = items[0];
+// new items are loaded by scrolling to the top and then scrolling back down to the bottom
+async function loadMoreItems(items) {
+  console.log('loading new messages...');
 
-  // we need to scroll away first otherwise the page won't load new messages
+  const firstItem = items[0];
   firstItem.scrollIntoView();
-  // nothing to load so we don't need to wait very long
   await delay(SCROLL_DELAY);
 
+  const lastItem = items[items.length - 1];
   lastItem.scrollIntoView();
-  await delay(LOAD_DELAY);
+  await longPoll(checkForNewItems, { timeout: LOAD_DELAY });
+}
+
+async function checkForNewItems() {
+  const items = selectAllOrError(selectors.item);
+  await delay(100);
+  const newItems = selectAllOrError(selectors.item);
+  return newItems.length > items.length;
 }
 
 function selectAllOrError(selector) {
@@ -79,6 +83,21 @@ function runSequentially(promises) {
   return promises.reduce((result, current) => {
     return result.then(current);
   }, Promise.resolve());
+}
+
+// calls a function over and over again until it returns true, or until the timeout
+async function longPoll(fn, { timeout }) {
+  const startTime = Date.now();
+
+  const success = await fn();
+  if (success) return true;
+
+  const endTime = Date.now();
+
+  const ellapsed = endTime - startTime + 1;
+  if (ellapsed >= timeout) return false;
+
+  await longPoll(fn, { timeout: timeout - ellapsed });
 }
 
 (async () => {
